e0b962798b243195392ac331fb6515c51b0e7507
Kajiwala Aryan Bhavesh <163291557+AryanKaji@users.noreply.github.com>
2025-05-14 01:58:37 -0700

    Update app.js

--- SHA256_web/app.js
+++ SHA256_web/app.js
@@ -1,12 +1,19 @@
 angular.module('hashApp', []).controller('HashController', function($scope) {
   $scope.inputText = "";
   $scope.hashedText = "";
+  $scope.error = "";
 
   $scope.generateHash = function() {
     if ($scope.inputText.trim() !== "") {
       $scope.hashedText = CryptoJS.SHA256($scope.inputText).toString();
     } else {
-      $scope.hashedText = "Please enter a valid string.";
+      $scope.error = "Please enter a valid string.";
     }
+  };
+
+  $scope.reset = function() {
+    $scope.inputText = "";
+    $scope.hashedText = "";
+    $scope.error = "";
   };
 });
